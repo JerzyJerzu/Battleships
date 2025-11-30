7d9b9b5eba7c66fec00495a8ff2aed39047a0e5e
JerzyJerzu <PJ@pawlik.info.pl>
2025-11-30 22:09:26 +0100

    Made ship a regular class

--- src/main/kotlin/model/Ship.kt
+++ src/main/kotlin/model/Ship.kt
@@ -1,6 +1,6 @@
 package model
 
-data class Ship(val size: Int, val head: Coordinate, val direction: Direction) {
+class Ship(val size: Int, val head: Coordinate, val direction: Direction) {
     val coordinates: Set<Coordinate> by lazy {
         (0 until size).map { i ->
             if (direction == Direction.HORIZONTAL) {
